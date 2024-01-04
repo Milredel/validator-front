93b9dddea65e783d0e898f33a8a23c6b052a8c90
Morgan Fromentin <morgan.fromentin@gmail.com>
2024-01-04 18:37:55 +0100

    introducing delete line action

--- src/app/common/utils.ts
+++ src/app/common/utils.ts
@@ -1,5 +1,9 @@
 export abstract class Utils {
 
+    static compareObjects(o1: any, o2: any): boolean {
+        return JSON.stringify(o1) === JSON.stringify(o2);
+    }
+
     static isEmpty(obj: any): boolean {
         return obj && Object.keys(obj).length === 0 && Object.getPrototypeOf(obj) === Object.prototype;
     }
--- src/app/input-content-viewer/input-content-viewer.component.ts
+++ src/app/input-content-viewer/input-content-viewer.component.ts
@@ -3,23 +3,26 @@ import { CommonModule } from '@angular/common';
 import { ValidationResponseType } from '../types/validation-response.type';
 import { MatTableModule } from '@angular/material/table';
 import { Utils } from '../common/utils';
+import { Line } from '../types/line.type';
+import { Reasons } from '../interfaces/reasons.interface';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
     selector: 'app-input-content-viewer',
     standalone: true,
-    imports: [CommonModule, MatTableModule],
+    imports: [CommonModule, MatTableModule, MatIconModule],
     templateUrl: './input-content-viewer.component.html',
 })
 export class InputContentViewerComponent {
 
     @Input() validationResponse: ValidationResponseType = {statusCode: 200};
 
-    data = []
-    reasons = {}
+    data: Line[] = [] as unknown as Line[]
+    reasons: Reasons = {} as unknown as Reasons
 
     displayedColumns: string[] = ['date', 'label', 'amount']
 
-    dataSource = []
+    dataSource: Line[] = [] as unknown as Line[]
 
     ngOnChanges(changes: SimpleChanges) {
         if (changes['validationResponse']) {
@@ -32,5 +35,15 @@ export class InputContentViewerComponent {
     }
 
     hasErrors = (): boolean => !Utils.isEmpty(this.reasons)
+
+    isDuplicate = (element: Line, type: 'balances' | 'movements'): boolean => {
+        const duplicates = this.reasons.duplicates[type]
+        if (!duplicates) return false
+        return duplicates.filter((line) => Utils.compareObjects(line, element)).length > 0
+    }
+
+    deleteLine = (element: Line): void => {
+        console.log(element)
+    }
 
 }
